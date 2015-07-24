fcb466ea431d66896c17d5a9780deb0ca0c3fae5
Marko Martin <marcus@gratex.com>
2015-07-24 10:22:31 +0200

    cssCheck implemented, apply stylesheet only when it conforms to api.css

---
+++ www/cssCheck.js
@@ -0,0 +1,67 @@
+function cssCheck(expectedStyleSheet, realStyleSheet) {
+    // function checks if real is 'sublist' of expected
+    // usefull to allow or deny application of CSS in your component
+    // 'sublist' means:
+    // SELECTORS: styleSheet.rules[].selectorText
+    // - real cannot have any extra selectors only selectors in expected
+    // - order of selectors is significant
+    // RULE: if selectors are ok then each individual CSSStyleRule is checked for style
+    // - real.rule.style[] must be sublist of  expected.rule.style[] 
+
+    var expectedRules = _Array(expectedStyleSheet.rules);
+    var realRules = _Array(realStyleSheet.rules);
+
+    var expectedSelectorTexts = expectedRules.map(selectorText);
+    var realSelectorTexts = realRules.map(selectorText);
+
+    // console.log(expectedSelectorTexts, realSelectorTexts)
+    // SAMPLE: isSubList(["h1", ".selector1", ".selector2 span"],["h1", ".selector1", ".selector2 span"])
+    if (!isSubList(expectedSelectorTexts, realSelectorTexts)) {
+        return false;
+    }
+    var rulesOk = realRules.every(function(realRule) {
+        var expectedRule = find(expectedRules, function(expectedRule) {
+            return expectedRule.selectorText === realRule.selectorText;
+        });
+        // asrt(expectedRule!=null,"because:" selectorOk)
+        // names and order of rules must match
+        return expectedRule && isSubList(_Array(expectedRule.style), _Array(realRule.style));
+    });
+
+    return rulesOk;
+
+
+    function selectorText(r) {
+        return r.selectorText;
+    }
+
+    function isSubList(full, sequence) {
+        for (var f = 0, s = 0, fl = full.length, sl = sequence.length; s < sl && f < fl; full[f++] === sequence[s] && s++);
+        return full && s == sl;
+    }
+
+    function _Array(list) {
+        return Array.prototype.slice.call(list);
+    }
+
+    function find(_this, predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(_this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
